bbef0bea7bcb7639adc95614e04c71ac1dedcb04
Martin Wheldon <martin.wheldon@greenhills-it.co.uk>
2015-10-28 16:49:45 +0100

    Initial working version

---
+++ lib/Task/RequestTracker42.pm
@@ -0,0 +1,195 @@
+use strict;
+use warnings;
+package Task::RequestTracker42;
+
+=head1 NAME
+
+Task::RequestTracker42 - All of the modules required by Request Tracker 4.2 
+
+=head1 DESCRIPTION
+
+These are the modules that are required by Request Tracker v4.2:
+
+=over 4
+
+=item * Apache::Session
+
+=item * CGI::Cookie
+
+=item * CGI::Emulate::PSGI
+
+=item * CGI::PSGI
+
+=item * Class::Accessor::Fast
+
+=item * Convert::Color
+
+=item * Crypt::Eksblowfish
+
+=item * Crypt::SSLeay
+
+=item * Crypt::X509
+
+=item * CSS::Squish
+
+=item * Data::GUID
+
+=item * Data::ICal
+
+=item * Date::Extract
+
+=item * Date::Manip
+
+=item * DateTime::Format::Natural
+
+=item * DateTime::Locale
+
+=item * DBD::mysql
+
+=item * DBIx::SearchBuilder
+
+=item * Devel::GlobalDestruction
+
+=item * Devel::StackTrace
+
+=item * Digest::base
+
+=item * Digest::MD5
+
+=item * Digest::SHA
+
+=item * Email::Address
+
+=item * Email::Address::List        
+
+=item * FCGI::ProcManager
+
+=item * FCGI
+
+=item * File::Glob
+
+=item * File::ShareDir
+
+=item * File::Spec
+
+=item * File::Temp
+
+=item * File::Which
+
+=item * Getopt::Long
+
+=item * GnuPG::Interface
+
+=item * HTML::Entities
+
+=item * HTML::FormatText::WithLinks
+
+=item * HTML::FormatText::WithLinks::AndTables
+
+=item * HTML::Mason
+
+=item * HTML::Mason::PSGIHandler
+
+=item * HTML::Quoted
+
+=item * HTML::RewriteAttributes
+
+=item * HTML::Scrubber
+
+=item * HTTP::Message
+
+=item * IPC::Run3
+
+=item * List::MoreUtils
+
+=item * Locale::Maketext
+
+=item * Locale::Maketext::Fuzzy
+
+=item * Locale::Maketext::Lexicon
+
+=item * Log::Dispatch
+
+=item * LWP::Protocol::https
+
+=item * LWP::Simple
+
+=item * LWP::UserAgent  
+
+=item * Mail::Header
+
+=item * Mail::Mailer
+
+=item * MIME::Entity
+
+=item * MIME::Types
+
+=item * Module::Refresh
+
+=item * Module::Versions::Report
+
+=item * Mozilla::CA
+
+=item * Net::CIDR
+
+=item * Net::SSL
+
+=item * PerlIO::eol
+
+=item * Plack::Handler::Starlet
+
+=item * Pod::Usage
+
+=item * Regexp::Common
+
+=item * Regexp::Common::net::CIDR
+
+=item * Regexp::IPv6
+
+=item * Role::Basic
+
+=item * Scalar::Util
+
+=item * String::ShellQuote
+
+=item * Symbol::Global::Name
+
+=item * Sys::Syslog
+
+=item * Text::ParseWords
+
+=item * Text::Password::Pronounceable
+
+=item * Text::Quoted
+
+=item * Text::Template
+
+=item * Text::WikiFormat
+
+=item * Text::Wrapper
+
+=item * Time::HiRes
+
+=item * Time::ParseDate
+
+=item * Tree::Simple
+
+=item * UNIVERSAL::require
+
+=item * URI::QueryParam
+
+=item * XML::RSS
+
+=item * Apache::DBI
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Greenhills IT Ltd..
+
+You can distribute this module under the same terms as Perl itself.
+
+=cut
+
+1;
